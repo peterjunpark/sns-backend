507303b314f622d20d52f1dca71b89f2293457e8
Peter Park <peterjpark@pm.me>
2023-07-11 17:59:42 -0400

    Add controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,124 @@
-module.exports = {};
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all thoughts or get a thought by id.
+  getThought: async (req, res) => {
+    const { id } = req.body;
+    try {
+      if (id) {
+        // Get a thought by id
+        const thought = await Thought.findById(id).populate("reactions");
+        if (!thought) {
+          return res.status(404).json({ message: "Thought not found." });
+        }
+        res.json(thought);
+      } else {
+        // Get all thoughts
+        const thoughts = await Thought.find().populate("reactions");
+        res.json(thoughts);
+      }
+    } catch (error) {
+      console.error("Error getting thoughts:", error);
+      res.status(500).json({ error: "Failed to get thoughts." });
+    }
+  },
+
+  // Post a new thought (and update associated user's thoughts list).
+  postThought: async (req, res) => {
+    const { thoughtText, username } = req.body;
+    try {
+      const thought = await Thought.create({ thoughtText, username });
+      // Add thought to associated user's thoughts list
+      const user = await User.findOneAndUpdate(
+        { username },
+        { $push: { thoughts: thought._id } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.status(201).json(thought);
+    } catch (error) {
+      console.error("Error creating thought:", error);
+      res.status(500).json({ error: "Failed to create thought." });
+    }
+  },
+
+  // Update a thought by id.
+  updateThought: async (req, res) => {
+    const { id, thoughtText } = req.body;
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(
+        id,
+        { thoughtText },
+        { new: true }
+      );
+      if (!updatedThought) {
+        return res.status(404).json({ message: "Thought not found." });
+      }
+      res.json(updatedThought);
+    } catch (error) {
+      console.error("Error updating thought:", error);
+      res.status(500).json({ error: "Failed to update thought." });
+    }
+  },
+
+  // Delete a thought by id.
+  deleteThought: async (req, res) => {
+    const { id } = req.body;
+    try {
+      const deletedThought = await Thought.findByIdAndDelete(id);
+      if (!deletedThought) {
+        return res.status(404).json({ message: "Thought not found." });
+      }
+      // Remove thought from associated user's thoughts list
+      await User.findOneAndUpdate(
+        { username: deletedThought.username },
+        { $pull: { thoughts: deletedThought._id } }
+      );
+      res.json({ message: "Thought deleted successfully." });
+    } catch (error) {
+      console.error("Error deleting thought:", error);
+      res.status(500).json({ error: "Failed to delete thought." });
+    }
+  },
+
+  // Create a reaction and add it to an existing thought.
+  addReactionToThought: async (req, res) => {
+    const { id } = req.params;
+    const { reactionBody, username } = req.body;
+    try {
+      const thought = await Thought.findById(id);
+      if (!thought) {
+        return res.status(404).json({ message: "Thought not found." });
+      }
+      const reaction = { reactionBody, username };
+      thought.reactions.push(reaction);
+      const updatedThought = await thought.save();
+      res.status(201).json(updatedThought);
+    } catch (error) {
+      console.error("Error creating reaction:", error);
+      res.status(500).json({ error: "Failed to create reaction." });
+    }
+  },
+
+  // Delete a reaction from a thought by id.
+  deleteReactionFromThought: async (req, res) => {
+    const { id } = req.params;
+    const { reactionId } = req.params;
+    try {
+      const thought = await Thought.findById(id);
+      if (!thought) {
+        return res.status(404).json({ message: "Thought not found." });
+      }
+      thought.reactions = thought.reactions.filter(
+        (reaction) => reaction._id.toString() !== reactionId
+      );
+      const updatedThought = await thought.save();
+      res.json(updatedThought);
+    } catch (error) {
+      console.error("Error deleting reaction:", error);
+      res.status(500).json({ error: "Failed to delete reaction." });
+    }
+  },
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,121 @@
-module.exports = {};
+const { Thought, User } = require("../models");
+const { hashPassword } = require("../utils/helpers");
+
+module.exports = {
+  // Get all users or get a user by id.
+  getUser: async (req, res) => {
+    const { id } = req.body;
+    try {
+      if (id) {
+        // Get a user by id with populated thought and friend data.
+        const user = await User.findById(id)
+          .populate("thoughts")
+          .populate("friends");
+        if (!user) {
+          return res.status(404).json({ message: "User not found." });
+        }
+        res.json(user);
+      } else {
+        // Get all users
+        const users = await User.find()
+          .populate("thoughts")
+          .populate("friends");
+        res.json(users);
+      }
+    } catch (error) {
+      console.error("Error getting users:", error);
+      res.status(500).json({ error: "Failed to get users." });
+    }
+  },
+
+  // Post a new user.
+  postUser: async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+      const hashedPassword = await hashPassword(password);
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      res.status(201).json(user);
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).json({ error: "Failed to create user." });
+    }
+  },
+
+  // Update a user by id.
+  updateUser: async (req, res) => {
+    const { id, username, email, password } = req.body;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { username, email, password },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).json({ error: "Failed to update user." });
+    }
+  },
+
+  // Delete a user by id (and associated Thoughts).
+  deleteUser: async (req, res) => {
+    const { id } = req.body;
+    try {
+      const deletedUser = await User.findByIdAndDelete(id);
+      if (!deletedUser) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      // Delete user's thoughts
+      await Thought.deleteMany({ username: deletedUser.username });
+      res.json({ message: "User deleted successfully." });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({ error: "Failed to delete user." });
+    }
+  },
+
+  // Post a user to another user's friends list.
+  addFriendToUser: async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+      const user = await User.findByIdAndUpdate(
+        userId,
+        { $addToSet: { friends: friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error("Error adding friend to user:", error);
+      res.status(500).json({ error: "Failed to add friend to user." });
+    }
+  },
+
+  // Delete a user from another user's friends list.
+  deleteFriendFromUser: async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+      const user = await User.findByIdAndUpdate(
+        userId,
+        { $pull: { friends: friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error("Error deleting friend from user:", error);
+      res.status(500).json({ error: "Failed to delete friend from user." });
+    }
+  },
+};
